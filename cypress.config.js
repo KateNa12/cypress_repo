2a9143aabd25a4fea7936fcc3e40ee6d50800f2c
Kateryna Naimark <kateryna.naimark@gmail.com>
2025-01-02 11:48:22 +0200

    hw18.cy.js has been fixed

--- cypress.config.js
+++ cypress.config.js
@@ -4,11 +4,11 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       },
-    baseUrl: 'https://example.cypress.io',
-    retries: {
-      runMode: 1,
-      openMode: 1
-    },
+    baseUrl: 'https://guest:welcome2qauto@qauto.forstudy.space/',
+    // retries: {
+    //   runMode: 1,
+    //   openMode: 1
+    // },
     viewportHeight: 1080,
     viewportWidth: 1920,
     video: true,
--- cypress/e2e/1-getting-started/hw18.cy.js
+++ cypress/e2e/1-getting-started/hw18.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe('Перевірка хедерів та футерів на сайті', () => {
+describe('to check visibility and attributes of the headers and the footers on the website', () => {
     beforeEach(() => {
         cy.visit('https://qauto.forstudy.space', {
             auth: {
@@ -10,13 +10,13 @@ describe('Перевірка хедерів та футерів на сайті'
     })
 })
 
-it('Перевірка елементу Home', () => {
+it('To check visibility and attributes of the element Home at the header', () => {
     cy.get('a.btn.header-link').should('be.visible');
     cy.get('a.btn.header-link').should('have.text', 'Home');
     cy.get('a.btn.header-link').should('have.class', '-active')
 });
 
-it('Перевірка елементу About', () => {
+it('To check visibility and attributes of the element About at the header', () => {
     cy.get('[appscrollto="aboutSection"]').should('be.visible');
     // cy.contains('About').should('be.visible');
     cy.contains('About').should('have.text', 'About');
@@ -25,7 +25,7 @@ it('Перевірка елементу About', () => {
   });
 
 
-  it('Перевірка елементу Contacts', () => {
+  it('To check visibility and attributes of the element Contacts at the header', () => {
     cy.get('[appscrollto="contactsSection"]').should('be.visible');
     // cy.contains('Contacts').should('be.visible');
     cy.get('[appscrollto="contactsSection"]').should('have.text', 'Contacts');
@@ -33,14 +33,14 @@ it('Перевірка елементу About', () => {
     cy.get('[appscrollto="contactsSection"]').should('have.attr', 'appscrollto');
   });
 
-  it('Перевірка елементу Guest log in', () => {
+  it('To check visibility and attributes of the element Guest log in at the header', () => {
     cy.get('button.header-link.-guest').should('have.class', 'header-link -guest')
     cy.get('[class="header-link -guest"]').should('be.visible');
     cy.get('[class="header-link -guest"]').should('have.text', 'Guest log in');
     cy.get('[class="header-link -guest"]').should('be.visible').and('not.be.disabled').click();
   });
 
-  it('Перевірка елементу Sign In', () => {
+  it('To check visibility and attributes of the element Sign In at the header', () => {
     cy.get('button.btn.btn-outline-white.header_signin').should('have.class', 'btn btn-outline-white header_signin')
     cy.get('[class="btn btn-outline-white header_signin"]').should('be.visible');
     cy.get('[class="btn btn-outline-white header_signin"]').should('have.text', 'Sign In');
@@ -48,39 +48,35 @@ it('Перевірка елементу About', () => {
   });
 
 
-  it('Перевірка елементу Facebook', () => {
+  it('To check visibility and attributes of the element Facebook at the footer', () => {
     cy.get('[class="socials_icon icon icon-facebook"]').should('be.visible');
     cy.get('a[href="https://www.facebook.com/Hillel.IT.School"]').should('exist').and('be.visible');
     cy.get('.icon-facebook').parent().invoke('removeAttr','target').should('not.be.disabled').click();
   });
 
-  it('Перевірка елементу Telegram', () => {
+  it('To check visibility and attributes of the element Telegram at the footer', () => {
     cy.get('[class="socials_icon icon icon-telegram"]').should('be.visible');
     cy.get('a[href="https://t.me/ithillel_kyiv"]').should('exist').and('be.visible');
     cy.get('.icon-telegram').parent().invoke('removeAttr','target').should('not.be.disabled');
   });
     
 
-  it('Перевірка елементу YouTube', () => {
+  it('To check visibility and attributes of the element YouTube at the footer', () => {
     cy.get('[class="socials_icon icon icon-youtube"]').should('be.visible');
     // cy.get('a[href="https://www.youtube.com/user/HillelITSchool?sub_confirmation=1"]').should('exist').and('be.visible');
     cy.get('.icon-youtube').parent().invoke('removeAttr','target').should('not.be.disabled');
   });
    
-  it('Перевірка елементу Instagram', () => {
+  it('To check visibility and attributes of the element Instagram at the footer', () => {
     cy.get('[class="socials_icon icon icon-instagram"]').should('be.visible');
     cy.get('a[href="https://www.instagram.com/hillel_itschool/"]').should('exist').and('be.visible');
     cy.get('.icon-instagram').parent().invoke('removeAttr','target').should('not.be.disabled').click();
   }); 
     
-  it('Перевірка елементу Linkedin', () => {
+  it('To check visibility and attributes of the element Linkedin at the footer', () => {
     cy.get('[class="socials_icon icon icon-linkedin"]').should('be.visible');
     cy.get('a[href="https://www.linkedin.com/school/ithillel/"]').should('exist').and('be.visible');
     cy.get('.icon-linkedin').parent().invoke('removeAttr','target').should('not.be.disabled').click();
-  });
-
-
-
-
+  })
 
 })
